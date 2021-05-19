95f667c993643ca1ebcc555bf91d7cf0c41d3378
WardenZZZ <84342795+WardenZZZ@users.noreply.github.com>
2021-05-19 16:49:48 +0300

    Форма и база данных для работы

---
+++ Project/Project 22042021/Add.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace Project_22042021
+{
+    partial class Add
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Add));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Berlin Sans FB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(205, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Добавить";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Berlin Sans FB", 13F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(21, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 22);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "WIP 3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Berlin Sans FB", 13F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(14, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "WIP 1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(114, 63);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBox2.Location = new System.Drawing.Point(114, 106);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 63);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Добавить данные";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBox3.Location = new System.Drawing.Point(114, 149);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 26);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Berlin Sans FB", 13F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(17, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "WIP 2";
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.Font = new System.Drawing.Font("Berlin Sans FB", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(22, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 33);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Отмена";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Berlin Sans FB", 13F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(21, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 22);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "WIP 4";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBox4.Location = new System.Drawing.Point(114, 188);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(180, 26);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Berlin Sans FB", 13F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(21, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 22);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "WIP 50";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBox5.Location = new System.Drawing.Point(114, 225);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(180, 26);
+            this.textBox5.TabIndex = 13;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1 - Активное",
+            "2 - Неактивное",
+            "3 - На складе",
+            "4 - Ремонт",
+            "5 - Списано"});
+            this.comboBox2.Location = new System.Drawing.Point(114, 64);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(180, 24);
+            this.comboBox2.TabIndex = 15;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Brazil",
+            "Podolsk",
+            "Ukraine",
+            "Redgrave"});
+            this.comboBox3.Location = new System.Drawing.Point(114, 107);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(180, 24);
+            this.comboBox3.TabIndex = 15;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "1 - winrar",
+            "2 -Telegram",
+            "3 - Yahoo",
+            "4 - Dr.Web",
+            "5 - visual studio",
+            "6 - office",
+            "7 - ff14 online free trial",
+            "8 - Handbrake",
+            "9 - ANT-VIRUS"});
+            this.comboBox4.Location = new System.Drawing.Point(114, 150);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(180, 24);
+            this.comboBox4.TabIndex = 15;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(114, 181);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(114, 205);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 16;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // Add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(338, 320);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Add";
+            this.Text = "Добавление";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Add_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Add_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    }
+}
---
+++ Project/Project 22042021/Add.cs
@@ -0,0 +1,264 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Project_22042021
+{
+    public partial class Add : Form
+    {
+        DataGridView _dataGridView;
+        public Add(DataGridView dataGridView)
+        {
+            InitializeComponent();
+            this._dataGridView = dataGridView;
+
+            Addtry();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Addtry()
+        {
+            DB db = new DB();
+            System.Data.DataTable table = new System.Data.DataTable();
+            MySqlDataAdapter ad = new MySqlDataAdapter();
+            MySqlCommand prekol = new MySqlCommand(uwu.com, db.getConnection());
+            ad.SelectCommand = prekol;
+            ad.Fill(table);
+            _dataGridView.DataSource = table;
+
+            switch (uwu.perem)
+            {
+                case "pc acc":
+                    comboBox3.Items.Clear();                  
+                    comboBox2.Items.Clear();
+                    var name = new MySqlCommand("SELECT MAX(id_cond) FROM `cond`", db.getConnection());
+                    db.openConnection();
+
+                    var id = name.ExecuteScalar();
+                    for (int i = 1; i <= Convert.ToInt32(id); i++)
+                    {
+                        name = new MySqlCommand("SELECT condname FROM `cond` WHERE id_cond = " + i + "", db.getConnection());
+                        comboBox2.Items.Add(i + " - " + Convert.ToString(name.ExecuteScalar()));
+                    }
+
+                    name = new MySqlCommand("SELECT MAX(id_dept) FROM `dept`", db.getConnection());
+                    db.openConnection();
+
+                    id = name.ExecuteScalar();
+                    for (int i = 1; i <= Convert.ToInt32(id); i++)
+                    {
+                        name = new MySqlCommand("SELECT deptname FROM `dept` WHERE id_dept = " + i + "", db.getConnection());
+                        comboBox3.Items.Add(i + " - " + Convert.ToString(name.ExecuteScalar()));
+                    }
+                    comboBox4.Hide();
+                    label4.Location = new Point(41, 108);
+                    label2.Text = "Состояние";
+                    label4.Text = "Отдел";
+
+                    textBox1.Hide();
+                    textBox2.Hide();
+                    textBox3.Hide();
+                    textBox4.Hide();
+                    textBox5.Hide();
+                    label3.Hide();
+                    label5.Hide();
+                    label6.Hide();
+                    dateTimePicker1.Hide();
+                    dateTimePicker2.Hide();
+
+
+                    break;
+                case "SOFTWARE":
+                    comboBox4.Items.Clear();
+                    name = new MySqlCommand("SELECT MAX(id_typesoft) FROM `softtypes`", db.getConnection());
+                    db.openConnection();
+
+                    id = name.ExecuteScalar();
+                    for (int i = 1; i <= Convert.ToInt32(id); i++)
+                    {
+                        name = new MySqlCommand("SELECT Name FROM `softtypes` WHERE id_typesoft = " + i + "", db.getConnection());
+                        comboBox4.Items.Add(i + " - " + Convert.ToString(name.ExecuteScalar()));
+                    }
+                    
+                    label4.Location = new Point(50, 108);
+                    label2.Text = "Имя ПО";
+                    label4.Text = "ПК";
+                    label3.Text = "Тип ПО";
+                    label5.Text = "Срок\nлицензии";
+
+                    comboBox3.Hide();
+                    comboBox2.Hide();
+                    comboBox4.Show();
+                    textBox1.Show();
+                    textBox2.Show();
+                    textBox3.Show();
+                    textBox4.Hide();
+                    textBox5.Hide();
+                    label5.Show();
+                    label6.Hide();
+                    dateTimePicker1.Show();
+                    dateTimePicker2.Show();
+                    break;
+                case "HARDWARE":
+                    comboBox2.Items.Clear();
+                    comboBox3.Items.Clear();
+                    name = new MySqlCommand("SELECT MAX(id_hardtype) FROM `hardtypes`", db.getConnection());
+                    db.openConnection();
+
+                    id = name.ExecuteScalar();
+                    for (int i = 1; i <= Convert.ToInt32(id); i++)
+                    {
+                        name = new MySqlCommand("SELECT name FROM `hardtypes` WHERE id_hardtype = " + i + "", db.getConnection());
+                        comboBox2.Items.Add(i + " - " + Convert.ToString(name.ExecuteScalar()));
+                    }
+
+                    name = new MySqlCommand("SELECT MAX(id_cond) FROM `cond`", db.getConnection());
+                    db.openConnection();
+
+                    id = name.ExecuteScalar();
+                    for (int i = 1; i <= Convert.ToInt32(id); i++)
+                    {
+                        name = new MySqlCommand("SELECT condname FROM `cond` WHERE id_cond = " + i + "", db.getConnection());
+                        comboBox3.Items.Add(i + " - " + Convert.ToString(name.ExecuteScalar()));
+                    }
+                    label4.Location = new Point(12, 108);
+                    label2.Text = "Комплект";
+                    label4.Text = "Состояние";
+                    label3.Text = "Кол-во";
+                    label5.Text = "Модель";
+                    comboBox2.Show();
+                    comboBox3.Show();
+                    comboBox4.Hide();
+                    textBox3.Show();
+                    textBox4.Show();
+                    textBox5.Hide();
+                    label6.Hide();
+                    dateTimePicker1.Hide();
+                    dateTimePicker2.Hide();
+
+                    break;
+                case "PERI":
+                    comboBox2.Items.Clear();
+                    comboBox3.Items.Clear();
+                    name = new MySqlCommand("SELECT MAX(id_peritype) FROM `peritypes`", db.getConnection());
+                    db.openConnection();
+
+                    id = name.ExecuteScalar();
+                    for (int i = 1; i <= Convert.ToInt32(id); i++)
+                    {
+                        name = new MySqlCommand("SELECT name FROM `peritypes` WHERE id_peritype = " + i + "", db.getConnection());
+                        comboBox2.Items.Add(i + " - " + Convert.ToString(name.ExecuteScalar()));
+                    }
+
+                    name = new MySqlCommand("SELECT MAX(id_cond) FROM `cond`", db.getConnection());
+                    db.openConnection();
+
+                    id = name.ExecuteScalar();
+                    for (int i = 1; i <= Convert.ToInt32(id); i++)
+                    {
+                        name = new MySqlCommand("SELECT condname FROM `cond` WHERE id_cond = " + i + "", db.getConnection());
+                        comboBox3.Items.Add(i + " - " + Convert.ToString(name.ExecuteScalar()));
+                    }
+                    
+                    label4.Location = new Point(12, 108);
+                    label2.Text = "Название";
+                    label4.Text = "Состояние";
+                    label3.Text = "Кол-во";
+                    label5.Text = "Модель";
+                    comboBox2.Show();
+                    comboBox3.Show();
+                    comboBox4.Hide();
+                    textBox3.Show();
+                    textBox4.Show();
+                    textBox5.Hide();
+                    label6.Hide();
+                    dateTimePicker1.Hide();
+                    dateTimePicker2.Hide();
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (uwu.perem)
+            {
+                case "pc acc":
+                    uwu.query = " INSERT INTO `pc acc` (id_cond,id_dept) VALUES ( " + (comboBox2.SelectedIndex + 1) + ", " + (comboBox3.SelectedIndex + 1) +" )";
+                    break;
+                case "SOFTWARE":
+                    uwu.query = " INSERT INTO `software` (softname,id_pc,softtype,license_start,license_end) VALUES ( '" + textBox1.Text + "', " + textBox2.Text + ", " + (comboBox4.SelectedIndex + 1) + ", '" + dateTimePicker1.Text + "', '" + dateTimePicker2.Text + "' )";
+                    break;
+                case "HARDWARE":
+                    uwu.query = " INSERT INTO `hardware` (id_hardtype,id_cond,qty,model) VALUES ( " + (comboBox2.SelectedIndex + 1) + ", " + (comboBox3.SelectedIndex + 1) + ", " + textBox3.Text + ", '" + textBox4.Text + "' )";
+                    break;
+                case "PERI":
+                    uwu.query = " INSERT INTO `peri` (id_peritype,id_cond,qty,model) VALUES ( " + (comboBox2.SelectedIndex + 1) + ", " + (comboBox3.SelectedIndex + 1) + ", " + textBox3.Text + ", '" + textBox4.Text + "' )";
+                    break;
+            }
+            zapol();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void zapol()
+        {
+            DB db = new DB();
+            System.Data.DataTable table = new System.Data.DataTable();
+            MySqlDataAdapter ad = new MySqlDataAdapter();
+            MySqlCommand prekol = new MySqlCommand(uwu.query, db.getConnection());
+            ad.SelectCommand = prekol;
+
+            ad.Fill(table);
+
+            _dataGridView.DataSource = table;
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker2.CustomFormat = "yyyy-MM-dd";
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            textBox4.Text = dateTimePicker2.Text; 
+        }
+
+        private void Add_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        Point lastPoint;
+
+        private void Add_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+    }
+}
